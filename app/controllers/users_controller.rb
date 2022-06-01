40241d88f448641c967c23ad1e0f2b9ff2271ae8
EC2 Default User <ec2-user@ip-172-31-4-152.ec2.internal>
2022-06-01 07:39:12 +0000

    [Add]いいね機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_correct_user, {only: [:edit, :update]}
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path
+      redirect_to user_path(current_user)
       flash[:notice] = "You have updated user successfully."
     else
       render "edit"
