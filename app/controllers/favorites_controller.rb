9264e1994ccec4b9318ce795d565536c7a9d0fd1
EC2 Default User <ec2-user@ip-172-31-4-152.ec2.internal>
2022-06-04 07:14:12 +0000

    [Add]フォロー/フォロワー

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,14 +5,14 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.new(book_id: book.id)
     #binding.pry
     favorite.save
-    redirect_back(fallback_location: root_path)
+    redirect_to request.referer
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    redirect_to request.referer
   end
 
 end
